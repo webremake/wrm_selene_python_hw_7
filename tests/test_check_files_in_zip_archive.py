6db3eaabd315cdc862b96e9cfd3e0efdf61f7660
Heorhii Lazarevich <webremake@gmail.com>
2023-03-23 20:26:14 +0300

    create initial project structure

---
+++ tests/test_check_files_in_zip_archive.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser_for_files_download():
+    pass
+
+
+def prepare_files_and_check_data_for_test():
+    pass
+
+
+def test_check_files_in_zip_archive():
+    pass
