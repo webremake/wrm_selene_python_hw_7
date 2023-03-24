aaa3bbab7750b934b76ba7321750e58b97394140
Heorhii Lazarevich <webremake@gmail.com>
2023-03-24 21:54:29 +0300

    change pdf download link and selectors

--- tests/test_check_files_in_zip_archive.py
+++ tests/test_check_files_in_zip_archive.py
@@ -3,21 +3,21 @@ import requests
 import os
 from os.path import abspath, dirname, join
 
-from selene import browser, be
+from selene import browser, be, have
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 XLSX_DOWNLOAD_LINK = 'https://freetestdata.com/document-files/xlsx/'  # [role="button" ][href*="100KB_XLSX"]
-PDF_DOWNLOAD_LINK = 'https://freetestdata.com/document-files/pdf/'  # [role="button"][href*="100KB_PDF"]
+PDF_DOWNLOAD_LINK = 'https://laserscom.com/ru/products'  # [role="button"][href*="100KB_PDF"]
 CSV_DOWNLOAD_LINK = 'https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_200KB_CSV-1.csv'  # [role="button" ][href*="200KB_CSV"]
 
 BASE_DIR = dirname(dirname(abspath(__file__)))  # path to project root directory
 RESOURCES_DIR = join(BASE_DIR, 'resources')  # path to save files
 
 CSV_TEST_FILE_NAME = 'csv_test_file.csv'
-XLSX_TEST_FILE_NAME = 'xlsx_test_file.xlsx'
-PDF_TEST_FILE_NAME = 'pdf_test_file.pdf'
+# XLSX_TEST_FILE_NAME = 'xlsx_test_file.xlsx'
+# PDF_TEST_FILE_NAME = 'pdf_test_file.pdf'
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -48,11 +48,15 @@ def create_resources_for_tests():
     with open(csv_file_path, 'wb') as csv_test_file:
         csv_test_file.write(response.content)
 
-    browser.open(PDF_DOWNLOAD_LINK)
-    browser.element('[role=button][href*="100KB_PDF"]').should(be.clickable).click()
+    # browser.open(PDF_DOWNLOAD_LINK)
+    # browser.element('[role=button][href*="100KB_PDF"]').should(be.clickable).click()
 
     browser.open(XLSX_DOWNLOAD_LINK)
     browser.element('[role="button" ][href*="100KB_XLSX"]').should(be.clickable).click()
+
+    browser.open(PDF_DOWNLOAD_LINK)
+    browser.all('.views-field-title .field-content a').element_by(have.exact_text('LDS-505-FP-10'))\
+        .should(be.clickable).click()
 
     yield
     # удалить все файлы и директорию resources
